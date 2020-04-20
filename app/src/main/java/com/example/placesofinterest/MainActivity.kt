e5c7dc41f0ec55a0362b12973e8b3f762a65b929
Kasper Teunissen <kasper.teunissen@gmail.com>
2020-04-20 14:52:53 +0200

    final

--- app/src/main/java/com/example/placesofinterest/MainActivity.kt
+++ app/src/main/java/com/example/placesofinterest/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.placesofinterest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.*
+import com.example.placesofinterest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
+
     }
 }
